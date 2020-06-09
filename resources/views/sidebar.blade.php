54390ad51f7df224d1995c5b3f10d0c518956a28
Alessandro <grossano.alessandro@gmail.com>
2020-06-09 18:57:23 +0200

    Es. terminato

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,3 +1,16 @@
 <nav class="sidebar">
-  fdsfsdfsf
+  <div class="home">
+    <div class="menu"><a href="/">H</a></div>
+    <div class="espanso"><a href="/">HOME</a></div>
+  </div>
+  
+  <div class="about">
+    <div class="menu"><a href="/about">A</a></div>
+    <div class="espanso"><a href="/about">ABOUT</a></div>
+  </div>
+  <div class="contact">
+    <div class="menu"><a href="/contact">C</a></div>
+    <div class="espanso"><a href="/contact">CONTACT</a></div>
+  </div>
+  
 </nav>
