d8a6de0751e8ff688e80a8c6e1080502f5e011e7
Alessandro <grossano.alessandro@gmail.com>
2020-06-09 17:36:47 +0200

    Es. terminato

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('appLayout')
+@section('content')
+
+  <div class="content">
+    <div class="box">
+      <h1> {{$content}} </h1>
+    </div>
+    
+  </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
+<!-- GOAL: Come visto a lezione, riproporre un layout comune a 3 pagine comprensivo di header + navbar (sulla sinistra) + footer; complessita' di questi 3 componenti a piacere (va bene dalla scritta del <p> come visto questa mattina alla navbar di Amazon); le pagine dovranno invece definire un content (attraverso la tecnica vista yield/section/extends) come Home, About e Contact; anche in questo caso, la complessita' del content e' a piacere -->
+
 <?php
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+  $content = "INDEX";
+    return view('index', compact('content'));
+});
+
+Route::get('/about', function () {
+  $content = "ABOUT";
+  return view('index', compact('content'));
+});
+
+Route::get('/contact', function () {
+  $content = "CONTACT";
+  return view('index', compact('content'));
 });
